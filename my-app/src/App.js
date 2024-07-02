299a1cb37cb9788b4b40cb03718f1f4b95d97ea5
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-07-02 08:24:07 +0200

    Utilisation du component AppRouter dans App.js et clean code App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-import Header from './components/Header';
-import HomePage from './pages/HomePage';
+import AppRouter from './components/AppRouter';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <HomePage />
+      <AppRouter />
       <Footer />
     </div>
   );
