cc9ab9e1ce3647a98c1f0bfd9674b38d7acf89bc
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-07-03 22:44:57 +0200

    Modification du component NavBarLinks

--- my-app/src/components/NavBarLinks.jsx
+++ my-app/src/components/NavBarLinks.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import Image from "./Image";
 
-function NavBarLinks({ navClassName, navDirection, navPId, navImgClassName, navImgSrc,navImgAlt, navIcon }) {
+function NavBarLinks({ navClassName, navId, navDirection, navImgClassName, navImgSrc, navImgAlt, navIcon, navPId, Navtxt }) {
     return (
-        <NavLink className={navClassName} to={navDirection}>
-            <Image imgClassName={navImgClassName} imgSrc={navImgSrc} imgAlt={navImgAlt} /> 
+        <NavLink className={navClassName} id={navId} to={navDirection}
+        >
+            <Image imgClassName={navImgClassName} imgSrc={navImgSrc} imgAlt={navImgAlt} />
             <i className={navIcon}></i>
-            <p id={navPId}></p>
+            <p id={navPId}>{Navtxt}</p>
         </NavLink>
     )
 }
+
 
 export default NavBarLinks;
