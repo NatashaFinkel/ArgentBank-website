678cde1f9c360c25d8fab9a128e7528b435376f7
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-07-03 08:37:58 +0200

    Ajout de la page profil dans AppRouter

--- my-app/src/components/AppRouter.jsx
+++ my-app/src/components/AppRouter.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./Header";
 import HomePage from "../pages/HomePage";
 import SignInPage from "../pages/SignInPage";
+import ProfilePage from "../pages/ProfilePage";
 
 const AppRouter = () => {
     return (
@@ -11,6 +12,7 @@ const AppRouter = () => {
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/sign-in" element={<SignInPage />} />
+                <Route path="/user" element={<ProfilePage />} />
             </Routes>
         </Router>
     )
