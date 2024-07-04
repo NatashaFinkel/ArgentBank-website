b4cc5fb1b995c030025b7c8ac283363993990088
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-07-04 14:41:14 +0200

    Version primaire de la fonction d'authentification

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -6,17 +6,36 @@ import NavBarLinks from "./NavBarLinks";
 function Header() {
     const location = useLocation();
     const locationPathName = location.pathname;
+  //  console.log(locationPathName);
 
-    console.log(locationPathName);
+    function authentification() {
+      //  const anonymousUser = document.getElementById("anonymous-user");
+     //   const signOutBtn = document.getElementById("signOut-btn");
+
+        switch (locationPathName) {
+            case "/user":
+                // l'authentification avec Redux sera là.
+                break;
+            default:
+                // l'authentification avec Redux sera là.
+                break;
+        }
+    }
+
+    authentification();
+    setTimeout(authentification, 100);
+    clearTimeout(setTimeout);
+
     return (
         <nav className="main-nav">
             <NavBarLinks navClassName="main-nav-logo" navDirection="/" navImgClassName="main-nav-logo-image" navImgSrc={headerImg} navImgAlt="Argent Bank Logo" />
             <div className="user-name-div">
+
                 <NavBarLinks navClassName="main-nav-item"
                     navDirection="/sign-in" navId="anonymous-user" navIcon="fa fa-user-circle nav-user-icon" navPId="userNameAndIcon" Navtxt="Sign in" />
 
                 <NavBarLinks navClassName="main-nav-item signOut-btn"
-                    navDirection="/" navIcon="fa fa-sign-out signOut-icon" id="signOut-btn" Navtxt="Sign Out" />
+                    navDirection="/" navIcon="fa fa-sign-out signOut-icon" navId="signOut-btn" Navtxt="Sign Out" />
             </div>
         </nav >
     )
