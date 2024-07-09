bc64e1bf2a510dcc4f0ac7e6719302bc05e2c685
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-07-09 20:27:20 +0200

    Ajout du paramètre NavOnClick dans NavBarLinks

--- my-app/src/components/NavBarLinks.jsx
+++ my-app/src/components/NavBarLinks.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import Image from "./Image";
 
-function NavBarLinks({ navClassName, navId, navDirection, navImgClassName, navImgSrc, navImgAlt, navIcon, navPId, Navtxt }) {
+function NavBarLinks({ navClassName, navId, navDirection, navImgClassName, navImgSrc, navImgAlt, navIcon, navPId, Navtxt, NavOnClick }) {
     return (
         <NavLink className={navClassName} id={navId} to={navDirection}
-        >
+            onClick={NavOnClick} >
             <Image imgClassName={navImgClassName} imgSrc={navImgSrc} imgAlt={navImgAlt} />
             <i className={navIcon}></i>
             <p id={navPId}>{Navtxt}</p>
