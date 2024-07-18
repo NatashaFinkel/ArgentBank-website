b38bdc88efc1eb0bb53ef842db47a9587c081d25
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-07-18 20:27:32 +0200

    Création du reducer profileSlice

---
+++ my-app/src/reducers/profileSlice.js
@@ -0,0 +1,61 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const getProfile = createAsyncThunk(
+    'user/profile',
+    async ({ firstName, lastName, email, password, token }, thunkAPI) => {
+        try {
+            const response = await fetch('http://localhost:3001/api/v1/user/profile', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify({ firstName, lastName, email, password, token }),
+            });
+
+            const data = await response.json();
+            if (response.ok) {
+                const dataContent = data.body;
+                console.log(dataContent);
+                localStorage.setItem('userinfo', JSON.stringify({ firstName, lastName, email, password, token }));
+                return { ...dataContent };
+            } else {
+                return thunkAPI.rejectWithValue(data);
+            }
+        } catch (error) {
+            return thunkAPI.rejectWithValue({ message: error.message });
+        }
+    }
+);
+
+const initialState = { firstName: '', lastName: '' };
+
+const userProfile = createSlice({
+    name: 'profile',
+    initialState,
+    reducers: {
+        getProfile: () => { },
+        getProfileFulfilled: (state, action) => {
+            const { profile } = action.payload;
+            return [...state, profile.firstName, profile.lastName]
+        },
+        getProfileRejected: (state, action) => {
+            return state;
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(getProfile.fulfilled, (state, action) => {
+                const { firstName, lastName } = action.payload;
+                state.firstName = firstName;
+                state.lastName = lastName;
+            })
+            .addCase(getProfile.rejected, (state) => {
+                return state;
+            });
+    }
+});
+
+export const { getProfileFulfilled, getProfileRejected } = userProfile.actions;
+export { getProfile };
+export default userProfile.reducer;
