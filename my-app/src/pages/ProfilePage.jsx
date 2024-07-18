958fdd2a8f1ff71dcf01f5a01708f3d66563b749
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-07-18 08:13:07 +0200

    Amélioration du composant ProfilePage

--- my-app/src/pages/ProfilePage.jsx
+++ my-app/src/pages/ProfilePage.jsx
@@ -1,19 +1,31 @@
 import React from "react";
 import Button from "../components/Button";
 import Account from "../components/Account";
+import Header from "../components/Header";
 
-function ProfilePage() {
+function ProfilePage({ userFirstName, userLastName, checkBalance, savingsBalance, creditCardBalance }) {
+
+    userFirstName = "Luke"
+    userLastName = "Skywalker";
+
+    checkBalance = "2,082.79";
+    savingsBalance = "10,928.42";
+    creditCardBalance = "184.30";
+
     return (
-        <main className="main bg-dark">
-            <div className="header">
-                <h1>Welcome back<br />Tony Jarvis!</h1>
-                <Button btnClassName="edit-button" btnTxt="Edit Name" />
-            </div>
-            <h2 className="sr-only">Accounts</h2>
-            <Account title="Argent Bank Checking (x8349)" amount="$2,082.79" description="Available Balance" />
-            <Account title="Argent Bank Savings (x6712)" amount="$10,928.42" description="Available Balance" />
-            <Account title="Argent Bank Credit Card (x8349)" amount="$184.30" description="Current Balance" />
-        </main>
+        <div>
+            <Header />
+            <main className="main bg-dark">
+                <div className="header">
+                    <h1 id="user-name">Welcome back<br />{userFirstName + userLastName}</h1>
+                    <Button btnClassName="edit-button" btnTxt="Edit Name" />
+                </div>
+                <h2 className="sr-only">Accounts</h2>
+                <Account title="Argent Bank Checking (x8349)" amount={checkBalance} description="Available Balance" id="check-balance" />
+                <Account title="Argent Bank Savings (x6712)" amount={savingsBalance} description="Available Balance" id="savings-balance" />
+                <Account title="Argent Bank Credit Card (x8349)" amount={creditCardBalance} description="Current Balance" id="credit-card-balance" />
+            </main>
+        </div>
     )
 }
 
