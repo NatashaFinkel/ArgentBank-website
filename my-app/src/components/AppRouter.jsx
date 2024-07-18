31e61e9ba18960b49d046653a7cb95b4eb0b79db
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-07-18 08:09:29 +0200

    Ajout du Header dans AppRouter et changement du nom de la page User en ProfilePage

--- my-app/src/components/AppRouter.jsx
+++ my-app/src/components/AppRouter.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./Header";
 import HomePage from "../pages/HomePage";
 import SignInPage from "../pages/SignInPage";
 import ProfilePage from "../pages/ProfilePage";
@@ -8,11 +7,11 @@ import ProfilePage from "../pages/ProfilePage";
 const AppRouter = () => {
     return (
         <Router>
-            <Header />
+           
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/sign-in" element={<SignInPage />} />
-                <Route path="/user" element={<ProfilePage />} />
+                <Route path="/profile" element={<ProfilePage />} />
             </Routes>
         </Router>
     )
