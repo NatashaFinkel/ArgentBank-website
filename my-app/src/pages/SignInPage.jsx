426ecdfda939414b7956be12efff255c8087402c
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-07-18 08:45:01 +0200

    Modification de SignInPage

--- my-app/src/pages/SignInPage.jsx
+++ my-app/src/pages/SignInPage.jsx
@@ -1,9 +1,10 @@
 import { React, useState } from "react";
+import Header from '../components/Header';
 import { useDispatch, useSelector } from "react-redux";
-import { loginAsync } from "../reducers/authentificationSlice.js";
+import { loginAsync } from "../reducers/authenticationSlice";
 import { useNavigate } from 'react-router-dom';
 
-function SignInPage() {
+const SignInPage = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const [email, setEmail] = useState('');
@@ -15,36 +16,39 @@ function SignInPage() {
         e.preventDefault();
         const resultAction = await dispatch(loginAsync({ email, password }));
         if (resultAction.payload && resultAction.payload.isAuthenticated) {
-            navigate('/user');
+           navigate('/profile');
         } else {
             console.log('Login failed:', resultAction.payload);
         }
     };
     return (
-        <main className="main bg-dark">
-            <section className="sign-in-content">
-                <i className="fa fa-user-circle sign-in-icon"></i>
-                <h1>Sign In</h1>
-                <form onSubmit={handleLogin}>
-                    <div className="input-wrapper">
-                        <label htmlFor="userName">Username</label
-                        ><input type="email" id="userName" value={email} onChange={(e) => setEmail(e.target.value)} autoComplete="on" required />
-                    </div>
-                    <div className="input-wrapper">
-                        <label htmlFor="password">Password</label
-                        ><input type="password" id="password" value={password} autoComplete="on" onChange={(e) => setPassword(e.target.value)} required />
-                    </div>
-                    <div className="input-remember">
-                        <input type="checkbox" id="remember-me" />
-                        <label htmlFor="remember-me">Remember me</label>
-                    </div>
+        <div>
+            <Header />
+            <main className="main bg-dark">
+                <section className="sign-in-content">
+                    <i className="fa fa-user-circle sign-in-icon"></i>
+                    <h1>Sign In</h1>
+                    <form onSubmit={handleLogin}>
+                        <div className="input-wrapper">
+                            <label htmlFor="email">Username</label>
+                            <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} autoComplete="on" required />
+                        </div>
+                        <div className="input-wrapper">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" id="password" value={password} autoComplete="on" onChange={(e) => setPassword(e.target.value)} required />
+                        </div>
 
-                    <button className="sign-in-button" onClick={handleLogin} type="submit">Sign in</button>
-                    {isAuthenticated && <p>You are logged in!</p>}
-                </form>
-            </section>
-        </main>
+                        <div className="input-remember">
+                            <input type="checkbox" id="remember-me" />
+                            <label htmlFor="remember-me">Remember me</label>
+                        </div>
+                        <button className="sign-in-button" onClick={handleLogin} type="submit">Sign in</button>
+                        { isAuthenticated }
+                    </form>
+                </section>
+            </main>
+        </div>
     );
-}
+};
 
 export default SignInPage;
