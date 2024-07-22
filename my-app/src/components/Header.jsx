ff2e24b96a21e1c51ba72cd4b7ed75e81daf2029
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-07-22 20:37:45 +0200

    Clean code

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,30 +1,8 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
 import headerImg from "../img/argentBankLogo.png";
 import NavBarLinks from "./NavBarLinks";
 
 function Header() {
-    const location = useLocation();
-    const locationPathName = location.pathname;
-  //  console.log(locationPathName);
-
-    function authentification() {
-      //  const anonymousUser = document.getElementById("anonymous-user");
-     //   const signOutBtn = document.getElementById("signOut-btn");
-
-        switch (locationPathName) {
-            case "/user":
-                // l'authentification avec Redux sera là.
-                break;
-            default:
-                // l'authentification avec Redux sera là.
-                break;
-        }
-    }
-
-    authentification();
-    setTimeout(authentification, 100);
-    clearTimeout(setTimeout);
 
     return (
         <nav className="main-nav">
